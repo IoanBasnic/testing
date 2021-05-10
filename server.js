a2d628725a15c85bb28b2fc83a44eeb5dbbdd524
Basnic Ioan <basnic.ioan@gmail.com>
2021-05-10 20:24:21 +0300

    second commit

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-// function requireHTTPS(req, res, next) {
-//   // The 'x-forwarded-proto' check is for Heroku
-//   if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-//     return res.redirect('https://' + req.get('host') + req.url);
-//   }
-//   next();
-// }
-// const express = require('express');
-// const app = express();
-// app.use(requireHTTPS);
-//
-// app.use(express.static('./dist/Client'));
-//
-// app.get('/*', function(req, res) {
-//   res.sendFile('index.html', {root: './dist/Client'}
-// );
-// });
-//
-// app.listen(process.env.PORT || 8080);
-const PORT = process.env.port || '8080';
-app = express();
-app.set("port", port);
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+
+app.use(express.static('./dist/Client'));
+
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {root: './dist/Client'}
+);
+});
+
+app.listen(process.env.PORT || 8080);
