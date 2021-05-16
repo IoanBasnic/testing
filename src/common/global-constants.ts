701d7c6d6e96e16c0710a0fc925a6d437e830368
Basnic Ioan <basnic.ioan@gmail.com>
2021-05-16 19:18:11 +0300

    Added smth

--- src/common/global-constants.ts
+++ src/common/global-constants.ts
@@ -4,8 +4,8 @@ import {Injectable} from '@angular/core';
   providedIn: 'root',
 })
 export class GlobalConstants {
-  // public static apiURL = 'http://3.66.189.188:8080/';
-   public static apiURL = 'http://localhost:8080/';
+   public static apiURL = 'http://3.66.189.188:8080/';
+  //  public static apiURL = 'http://localhost:8080/';
   public productID = '';
   // tslint:disable-next-line:typedef
   getPublicID() {return this.productID; }
