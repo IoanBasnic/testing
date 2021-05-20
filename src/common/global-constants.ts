e45a9b9d31b55df087ae8d51593a3bffe779d85c
Basnic Ioan <basnic.ioan@gmail.com>
2021-05-21 00:49:28 +0300

    Added smth

--- src/common/global-constants.ts
+++ src/common/global-constants.ts
@@ -5,8 +5,9 @@ import {Injectable} from '@angular/core';
 })
 export class GlobalConstants {
   // public static apiURL = 'http://3.66.189.188:8080/';
+  // public static apiURL = 'https://omtiamt-1383005511.eu-central-1.elb.amazonaws.com:8080/';
+  public static apiURL = 'covidtector.tk:8080/';
   public static apiServiceServerURL = 'http://18.197.177.146:6868/';
-  public static apiURL = 'https://omtiamt-1383005511.eu-central-1.elb.amazonaws.com:8080/';
   public static apiLogout = 'https://omtiamt-heroku.herokuapp.com/';
   //  public static apiURL = 'http://localhost:8080/';
   // public static apiLogout = 'http://localhost:4200/';
